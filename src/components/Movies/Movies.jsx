17b6062205a95eab1ff9047bbae0d8d2240ce81a
Sidharth Sethi <techspirit@duck.com>
2022-08-06 18:53:44 +0530

    Fixed API Issue

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 function Movies() {
+  const { data } = useGetMoviesQuery();
+
   return (
     <div>Movies</div>
   );
